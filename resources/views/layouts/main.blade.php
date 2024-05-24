d327a3e15a145484c95030ade60e0a8e242c9165
FAmanca <fadlyamanca@gmail.com>
2024-05-24 16:31:44 +0700

    styling posts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/side.css') }}">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>PelitaTaras | {{ $title }}</title>
+
 </head>
 
 <body>
@@ -23,6 +25,7 @@
     </section>
 
     <script src="{{ asset('js/script.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-
-    @foreach ($posts as $post )
-    <article class="mb-5">
-        <h2>
-            <a href="/post/{{ $post->slug }}">{{ $post->title }}</a>
-        </h2>
-        <h5>By : {{ $post->author }}</h5>
-        <p>{{ $post->excerpt }}</p>
-    </article>
-    @endforeach
-
+    <h2>Blog Kami</h2>
+    {{-- <div class="container mx-auto py-20"> --}}
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        @foreach ($posts as $post)
+            <div class="col">
+                <div class="card">
+                    <img src="{{ asset('images/Arona.jpg') }}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <p class="card-text">By : {{ $post->author }}</p>
+                        <a class="selengkapnya" href="/post/{{ $post->slug }}">Selengkapnya -></a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
